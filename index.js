b47705e4666a8ef41f585798dd9fcd06c2c167d0
Renardo Williams <rennwill@gmail.com>
2023-03-14 14:35:58 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+
+    switch (flair){
+        case "||": return function (msg = "special"){ return `You are ||${msg}||!`}
+        default: return function (msg = "special"){ return `You are *${msg}*!`}
+    }
+
+    //if (flair == "*")
+   // {
+   //     return function (msg = "special"){ return `You are *${msg}*!`}
+   
+   // }
+//else if (flair == "||")
+    //{
+   //     return function (msg = "special"){ return `You are ||${msg}||!`}
+  //  }
+}
+
+let test = wrapAdjective()();
+console.log(test);
